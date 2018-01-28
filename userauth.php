f94003fbeb92e5e319e967d161f6d346887f0da8
stin91 <32367172+stin91@users.noreply.github.com>
2018-01-28 22:08:55 +0100

    Update userauth.php

--- userauth.php
+++ userauth.php
@@ -1,16 +1,21 @@
 <?php 
  
-
+/**
+ * Description: User authentication
+ * @author Prem Tiwari
+ */
+ 
 //include database connection file
 require_once 'config.php';
- $us=$_POST['username'];
+  $us=$_POST['username'];
  $pas=$_POST['password'];
 //define database object
 global $dbc;
- $q="SELECT Username, Password FROM utente WHERE
-Username='".$us."' && Password='".$pas."'";
  
-$stmt = $dbc->prepare($q);
+$stmt = $dbc->prepare("SELECT Username,Password FROM utente WHERE
+Username= :usa && Password= :pas");
+$stmt->bindParam(':usa', $us, PDO::PARAM_STR, 12);
+$stmt->bindParam(':pas', $pas, PDO::PARAM_STR, 12);
  
 $stmt->execute();
  
