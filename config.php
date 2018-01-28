71c582662d98cd90b95a06c483e34954c8f8acf0
stin91 <32367172+stin91@users.noreply.github.com>
2018-01-28 21:14:54 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,8 @@
 <?php
  
 //connect to MySql database
-try {
-    $dbc=new PDO('mysql:host=localhost;dbname=iot','root','marika')
-     or die("Unable to connect.");
-       
-    }
 
-catch(PDOException $e)
-    {
-      echo "Error: " . $e->getMessage();
-	   print_r('error');
-    }
+    $dbc=new PDO('mysql:host=localhost;dbname=iot','root','marika')
+     
  
 ?>
