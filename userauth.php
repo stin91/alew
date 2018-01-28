33b240446e792eae5a3d546fb2e3d6d25443b040
stin91 <32367172+stin91@users.noreply.github.com>
2018-01-28 21:26:56 +0100

    Update userauth.php

--- userauth.php
+++ userauth.php
@@ -10,9 +10,10 @@ require_once 'config.php';
  
 //define database object
 global $dbc;
+ $q="SELECT Username, Password FROM utente WHERE
+Username='".$_POST['username']."' && Password='". ($_POST['password'])."'";
  
-$stmt = $dbc->prepare("SELECT Username, Password FROM utente WHERE
-Username='".$_POST['username']."' && Password='". ($_POST['password'])."'");
+$stmt = $dbc->prepare($q);
  
 $stmt->execute();
  
