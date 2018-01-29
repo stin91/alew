112734eb411b223b4c12c2c3556b567d25a278ac
stin91 <32367172+stin91@users.noreply.github.com>
2018-01-29 17:11:51 +0100

    Update select.php

--- select.php
+++ select.php
@@ -1,14 +1,14 @@
 <?php
 error_reporting(0);
-header("Access-Control-Allow-Origin: *");   
-header("Content-Type: application/json; charset=UTF-8");  
-$conn = new PDO("mysql:host=localhost;dbname=iot", "root", "marika");   
-$result = $conn->query("SELECT Username,Password FROM utente");     
-$outp = ""; 
+header('Access-Control-Allow-Origin: *');   
+header('Content-Type: application/json; charset=UTF-8');  
+$conn = new PDO('mysql:host=localhost;dbname=iot', 'root', '');   
+$result = $conn->query('SELECT Username,Password FROM utente');     
+$outp = ''; 
 while($rs = $result->fetch()) {     
-    if ($outp != "") {$outp .= ",";}     
-    $outp .= '{"Username":"'  . $rs["Username"] . '",'; 
-    $outp .= '"Password":"'   . $rs["Password"]  . '"}'; 
+    if ($outp != '') {$outp .= '';}     
+    $outp .= "{'Username':'"  . $rs['Username'] . "',"; 
+    $outp .= "'Password':'"   . $rs["Password"]  . "'}"; 
     } 
 $outp ='{"details":['.$outp.']}'; 
 echo($outp); 
