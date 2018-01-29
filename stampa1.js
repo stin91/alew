3ac2c3bf79c98f70d2938890e0f9afb7c92f01ca
stin91 <32367172+stin91@users.noreply.github.com>
2018-01-30 00:50:51 +0100

    Delete stampa1.js

--- stampa1.js
+++
@@ -1,7 +0,0 @@
- var app2 = angular.module("myApp");
-        app.controller("selectController", function($scope, $http) {
-            $http.get("select1.php") 
-            .then(function(response) { 
-            $scope.content = response.data.details;       
-            });
-        });
