b10b1267b28002329f33059b03c96471a73875a4
stin91 <32367172+stin91@users.noreply.github.com>
2018-01-30 01:01:27 +0100

    Update stampa.js

--- stampa.js
+++ stampa.js
@@ -1,17 +1,17 @@
-  var app = angular.module("myApp");
-        app.controller("utentiCtrl", function($scope, $http) {
+ var app = angular.module("myApp");
+        app.controller("utentiCtrl", function nome($scope, $http) {
             $http.get("select.php") 
             .then(function(response) { 
             $scope.content = response.data.details;       
             });
         });
-		app.controller("impiantiCtrl", function($scope, $http) {
+		app.controller("impiantiCtrl", function cognome($scope, $http) {
             $http.get("select1.php") 
             .then(function(response) { 
             $scope.content = response.data.details;       
             });
         });
-		app.controller("SensoriCtrl", function($scope, $http) {
+		app.controller("SensoriCtrl", function data($scope, $http) {
             $http.get("select2.php") 
             .then(function(response) { 
             $scope.content = response.data.details;       
